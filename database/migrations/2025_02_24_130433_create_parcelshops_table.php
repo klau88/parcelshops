fa557a8bfd70b3c638fe8c06ddcafd1a0d53c92f
Kalok Lau <kalok@parcelpro.nl>
2025-02-26 23:07:39 +0100

    Migration update

--- database/migrations/2025_02_24_130433_create_parcelshops_table.php
+++ database/migrations/2025_02_24_130433_create_parcelshops_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('external_id');
             $table->string('name');
             $table->string('slug');
+            $table->string('carrier');
             $table->string('type');
             $table->string('street');
             $table->string('number');
